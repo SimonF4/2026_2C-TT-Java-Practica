5bba957385bdd2254d8048b6d90118d751543be9
SimonF4 <simonetti_francoandres@hotmail.com>
2026-09-17 23:09:11 -0300

    Commit to initiate project

---
+++ HolaMundo/src/com/techlab/inicio/HolaMundo.java
@@ -0,0 +1,9 @@
+package com.techlab.inicio;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo este es mi 1er programa en la nueva computadora 2026! :D");
+	}
+
+}
