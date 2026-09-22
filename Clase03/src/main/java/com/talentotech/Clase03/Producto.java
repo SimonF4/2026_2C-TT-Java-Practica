7fad27efca1f5b97edfeaad07a612340cdd7fdbd
SimonF4 <simonetti_francoandres@hotmail.com>
2026-09-21 22:09:10 -0300

    M - Completar atributos y constructores

--- Clase03/src/main/java/com/talentotech/Clase03/Producto.java
+++ Clase03/src/main/java/com/talentotech/Clase03/Producto.java
@@ -2,30 +2,42 @@ package com.talentotech.Clase03;
 
 public class Producto {
 
+	// contador estatico:
+	// Funciona a nivel de clase, no depende de una instancia creada.
+	private static Long contadorId = 0L; 
+	
 	// Atributos
 	private Long id;
 	private String nombre;
-	private double precio;
+	private double precio; // No uso el wrapper en mi caso.
 	private int stock;
 	
-	public Producto (String nombre, double precio, int stock) {
-		this.nombre = nombre;
-		this.precio = precio;
-		this.stock = stock;
-	}
-
-	public Producto(long id, String nombre, double precio, int stock) {
-		this.id = id;
-		//this (nombre, precio, stock);
-		this.nombre = nombre;
-		this.precio = precio;
-		this.stock = stock;
-	}
-
 	public Producto() {
 		// Constructor default.
+		// Le agregamos valores default/genericos/de inicio.
+		this("Producto Generico", 0.0, 0);
+	}
+	
+	public Producto (String nombre, double precio) {
+		// Yo en mi caso aprovecho el otro constructor (con "this") para nada mas no repetir codigo.
+		this(nombre, precio, 1);
 	}
 
+	public Producto(String nombre, double precio, int stock) {
+		//this (nombre, precio, stock);
+		this.id = ++contadorId;
+		this.nombre = nombre;
+		this.precio = precio;
+		
+		if (this.stock == 0) {
+			this.stock = 1;
+		}else {
+			this.stock = stock;			
+		}
+		// TODO Mejorar: Validaciones, extraerlas a metodos y validar los otros atributos.
+	}
+
+	// GETTERS Y SETTERS
 	public String getNombre() {
 		return nombre;
 	}
